693d68493aa00f0ea61e952c7d939479de6fbbd1
Zr0AM <103156517+Zr0AM@users.noreply.github.com>
2022-06-21 21:24:03 -0500

    Update Sandbox1.java

--- workspaceRina/src/packageSandbox/Sandbox1.java
+++ workspaceRina/src/packageSandbox/Sandbox1.java
@@ -4,7 +4,7 @@ public class Sandbox1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Hello again!");
+		//System.out.println("Hello again!");
 
 	}
 
